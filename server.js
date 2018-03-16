914af5aea175531325ec1b24ffeebc9d56431489
A. Thompson <act.webdesigns@gmail.com>
2018-03-15 20:32:30 -0500

    server responding with better array of OBJs

--- server.js
+++ server.js
@@ -7,44 +7,29 @@ const PORT = process.env.PORT || 3001;
 //app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-/*function allProjects() {*/
-//db.Projects.findAll({
-//include: [
-//{
-//model: db.Users,
-//},
-//],
-//}).then(res => {
-//console.log(res);
-//let projects = () => {
-
-/*for (let project of res) {*/
-//console.log('-------------------------------------');
 //console.log(project.project_name);
 //console.log(project.project_desc);
 //console.log(project.project_lang);
 //console.log(project.User.firstname + ' ' + res[0].User.lastname);
-/*}*/
-/*}*/
-//});
-/*};*/
 
 db.sequelize.sync({force: false}).then(function() {
   app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
     app.get('/api', () => {
-      db.Projects.findAll().then(projects => {
-        projects.forEach(project => {
-          let projectArr = {};
-/*          console.log(project.dataValues);
-          console.log(project.project_name);
-          console.log(
-            '=============================================================',
-          ); */
-          projectArr = project.dataValues;
-          console.log(projectArr)
-          return projectArr;
+      db.Projects.findAll().then(res => {
+        let resObj = res.map(val => {
+          return {
+            id: val.id,
+            title: val.project_name,
+            category: val.category,
+            lang: val.project_lang,
+            desc: val.project_desc,
+            userId: val.UserId,
+            createdDate: val.createdAt,
+          };
         });
+        console.log(resObj);
+        return res;
       });
     });
   });
