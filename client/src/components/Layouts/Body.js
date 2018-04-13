8d27552b6e6c10c22c9a1717bd2974bd8b57d3fe
A. Thompson <act.webdesigns@gmail.com>
2018-04-13 17:47:21 -0500

    Router working. TODO: Start making GUI for CRUD -- Fix active menu button bug

--- client/src/components/Layouts/Body.js
+++ client/src/components/Layouts/Body.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {
-  Container,
-  Grid,
-} from 'semantic-ui-react';
+import {Container, Grid} from 'semantic-ui-react';
 
 import {ProjectForm, Projects} from '../Util';
 import {NavBar, NavMenu, Profile} from '../Layouts';
@@ -14,29 +11,32 @@ const styles = {
 
 class Body extends Component {
   state = {
-     activeItem: 'home',  
-  }
-  handleItemClick = (e, { name }) => this.setState({activeItem: name})
+    activeItem: '',
+  };
+  handleItemClick = (e, {name}) => this.setState({activeItem: name});
 
   render() {
     return (
-      <Container fluid style={styles}>
-        <NavBar />
-        <Grid>
-          <Grid.Column width={4}>
-            <NavMenu handleItemClick={this.handleItemClick} activeItem={this.state.activeItem} />
-          </Grid.Column>
-          <Grid.Column stretch="true" width={12}>
-          <Router>
-            <Switch>
-              <Route path="/" exact component={Profile} />
-              <Route path="/add-project" component={ProjectForm} />
-              <Route path="/project/:id" component={Projects} />
-            </Switch>
-          </Router>
-          </Grid.Column>
-        </Grid>
+      <Router>
+        <Container fluid style={styles}>
+          <NavBar />
+          <Grid>
+            <Grid.Column width={3}>
+              <NavMenu
+                handleItemClick={this.handleItemClick}
+                activeItem={this.state.activeItem}
+              />
+            </Grid.Column>
+            <Grid.Column stretch="true" width={12}>
+              <Switch>
+                <Route path="/" exact component={Projects} />
+                <Route path="/add-project" component={ProjectForm} />
+                <Route path="/projects" component={Profile} />
+              </Switch>
+            </Grid.Column>
+          </Grid>
         </Container>
+      </Router>
     );
   }
 }
--- client/src/components/Layouts/NavMenu.js
+++ client/src/components/Layouts/NavMenu.js
@@ -1,26 +1,40 @@
 import React, {Component} from 'react';
 import {Menu} from 'semantic-ui-react';
+import {NavLink} from 'react-router-dom';
 
 class NavMenu extends Component {
   render() {
-    const { activeItem, handleItemClick } =  this.props;
+    const {activeItem, handleItemClick} = this.props;
+
     return (
-      <Menu fluid vertical secondary >
-        <Menu.Item
-          name="profile"
-          active={activeItem === 'profile'}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name="projects"
-          active={activeItem === 'projects'}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name="explore"
-          active={activeItem === 'explore'}
-          onClick={handleItemClick}
-        />
+      <Menu fluid vertical secondary>
+        <NavLink to="/">
+          <Menu.Item
+            name="explore"
+            active={activeItem === 'explore'}
+            onClick={handleItemClick}>
+            Explore
+          </Menu.Item>
+        </NavLink>
+
+        <NavLink to="/add-project">
+          <Menu.Item
+            name="projects"
+            active={activeItem === 'projects'}
+            onClick={handleItemClick}>
+            Add Project
+          </Menu.Item>
+        </NavLink>
+
+        <NavLink to="/profile">
+          <Menu.Item
+            name="profile"
+            active={activeItem === 'profile'}
+            onClick={handleItemClick}>
+            {' '}
+            Profile
+          </Menu.Item>
+        </NavLink>
       </Menu>
     );
   }
